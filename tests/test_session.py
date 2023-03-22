68a9b316fb9c5e052096267194b228b8d0cbbfb0
BadPythonCoder <xsare435@gmail.com>
2023-03-22 16:07:40 +0000

    stuff

---
+++ tests/test_session.py
@@ -0,0 +1,16 @@
+from wrtsapi.Session import Session
+import json
+
+f = open("CREDS.txt")
+c = json.loads(f.read())
+f.close()
+
+def test():
+	session = Session(c["user"], c["pass"])
+	questions = session.get_questions()
+	q = next(questions)
+	print(q.title)
+	question = session.get_question(243445)
+	question.answer("bedankt allemaal!")
+	print("answered")
+	assert False
