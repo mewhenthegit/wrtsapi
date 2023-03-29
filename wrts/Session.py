d00f7876b3e53920cbdce875e33525c9cc8f5334
BadPythonCoder <xsare435@gmail.com>
2023-03-29 19:42:37 +0100

    Pre-release 0.1.0 FR

--- wrts/Session.py
+++ wrts/Session.py
@@ -9,8 +9,12 @@ class Session:
 	renew = 0
 	expiration = 0
 	token = ""
-	def __init__(self, user, passwd):
+	subjects = {}
+	def __init__(self, user, passwd, auto_gen_subjects=True):
 		self._login(user, passwd)
+		self.update_subjects()
+	def update_subjects(self):
+		resp = requests.get("https://api.wrts.nl/api/v3/subjects",headers={"x-auth-token": self.token})
 	def _login(self, user, passwd):
 		resp = requests.post("https://api.wrts.nl/api/v3/auth/get_token", json={"email": user, "password": passwd}).json()
 		#print(resp)
@@ -21,7 +25,7 @@ class Session:
 		self.expiration = resp["expires_at"]
 		self.token = resp["auth_token"]
 	def get_questions(self):
-		resp = requests.get("https://api.wrts.nl/api/v3/public/qna/questions").json()
+		resp = requests.get("https://api.wrts.nl/api/v3/public/qna/questions", headers={"x-auth-token": self.token}).json()
 		def gen(questions, token):
 			for q in questions:
 				yield Question(q["id"],self.token)
@@ -30,5 +34,5 @@ class Session:
 		return Question(id, self.token)
 	def post_question(self, contents, subject, attachments=[]):
 		data = {"contents": contents, "subject_id": subject, "qna_attachments_attributes": attachments}
-		resp = requests.post("https://api.wrts.nl/api/v3/public/qna/questions",json=json.dumps({"qna_question":data})).json()
+		resp = requests.post("https://api.wrts.nl/api/v3/public/qna/questions",headers={"x-auth-token": self.token},json=json.dumps({"qna_question":data})).json()
 		return get_question(resp["id"])
